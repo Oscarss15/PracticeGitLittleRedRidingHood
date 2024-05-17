c5d4f9b2d84c3ff57ff3e203b747532a4a21bfbc
Oscar <oscar15-91@hotmail.com>
2024-05-17 22:29:14 +0200

    Creacion de botones y capitulo1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
 
+
+let cont=0;
+
+function continueStory() {
+    cont++;
+
+    if(cont===1){
+        document.getElementById("textmain").style.display = "none";
+        document.getElementById("textstory1").style.display = "block";
+        document.getElementById("redhoodmain").style.display = "none";
+        document.getElementById("redhood1").style.display = "block";
+    }
+
+  }
+
+  function resetStory() {
+    cont=0;
+    document.getElementById("button").innerHTML="Continue story";
+    document.getElementById("textmain").style.display = "block";
+    document.getElementById("textstory1").style.display = "none";
+   
+    document.getElementById("redhoodmain").style.display = "block";
+    document.getElementById("redhood1").style.display = "none";
+    
+  }
+
+  
